f57c2f494ec11274de995cc7b927da5b7eae1372
=yennle <=phiyen263@gmail.com>
2021-06-24 16:36:17 -0400

    First Front End Commit

---
+++ front-end/src/routes.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import $ from 'jquery';
+
+window.jQuery = $;
+window.$ = $;
+global.jQuery = $;
+
+//Home
+const Home = React.lazy(() => import('./HMC/Home/Home'));
+
+//Appointment
+const MakeAppointment = React.lazy(() => import('./HMC/Appointment/MakeAppointment'));
+const Calendar = React.lazy(() => import('./HMC/Appointment/Calendar'));
+
+//Medical Instruction
+    //Home Health Testing
+const Measurement = React.lazy(() => import('./HMC/HomeHealthTesting/Measurement'));
+const MedicalTest = React.lazy(() => import('./HMC/HomeHealthTesting/MedicalTest'));
+const Visualization = React.lazy(() => import('./HMC/HomeHealthTesting/Visualization/Visualization'));
+    //Medication
+const Prescription = React.lazy(() => import('./HMC/Medication/Prescription'));
+const Reminder = React.lazy(() => import('./HMC/Medication/Reminder'));
+
+//HealthMonitor
+const Pulse = React.lazy(() => import('./HMC/HealthMonitor/Pulse'));
+const ReportSymptom = React.lazy(() => import('./HMC/HealthMonitor/ReportSymptom'));
+
+
+
+const routes = [
+    { path: '/home', exact: true, name: 'Home', component: Home },
+    { path: '/appointment/make-appointment', exact: true, name: 'Make Appointment', component: MakeAppointment },
+    { path: '/appointment/calendar', exact: true, name: 'Appointment Calendar', component: Calendar },
+    { path: '/home-test/add-measurement', exact: true, name: 'Measurement', component: Measurement },
+    { path: '/home-test/visualization', exact: true, name: 'Visualization', component: Visualization },
+    { path: '/home-test/add-test', exact: true, name: 'Medical Test', component: MedicalTest },
+    { path: '/medication/prescription', exact: true, name: 'Prescription', component: Prescription },
+    { path: '/medication/reminder', exact: true, name: 'Reminder', component: Reminder },
+    { path: '/health-monitor/pulse', exact: true, name: 'Pulse Monitor', component: Pulse },
+    { path: '/health-monitor/report-symptoms', exact: true, name: 'Report Symptoms', component: ReportSymptom },
+
+];
+
+export default routes;
