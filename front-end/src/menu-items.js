fa3d7ba9ac14b8b44633a8a134e8d245ba7da9ea
=yennle <=phiyen263@gmail.com>
2021-08-13 11:28:57 -0400

    Body Mesurement on menu

--- front-end/src/menu-items.js
+++ front-end/src/menu-items.js
@@ -121,18 +121,18 @@ export default {
             title: 'Upcoming',
             type: 'label-success'
           }
+        },
+        {
+          id: 'add-measurement',
+          title: 'Measurement',
+          type: 'item',
+          icon: 'feather icon-edit-2',
+          url: '/body-measurement',
+          badge: {
+            title: 'Upcoming',
+            type: 'label-success'
+          }
         }
-        // {
-        //   id: 'add-measurement',
-        //   title: 'Measurement',
-        //   type: 'item',
-        //   icon: 'feather icon-edit-2',
-        //   url: '/body-measurement',
-        //   badge: {
-        //     title: 'Upcoming',
-        //     type: 'label-success'
-        //   }
-        // },
       ]
     }
   ]
